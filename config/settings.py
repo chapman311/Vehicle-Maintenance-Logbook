2c4813df0108e7ba3d5eb4f8f66b4361e9fce4e8
chapman311 <fixerchapman@gmail.com>
2023-03-25 19:09:47 -0700

    Got Vehicle Maintenance Logbook up and running locally

--- config/settings.py
+++ config/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'uvxz^98l0q2=7v*3#+*e$gb7r%#ek+zqrlk#y_xdl(-vij&hcu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = boolify(config('DEBUG'))
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
 
@@ -135,9 +135,9 @@ LOGIN_URL = 'user-login/'
 
 
 
-if DEBUG == False:
-    import django_on_heroku
-    django_on_heroku.settings(locals())
+# if DEBUG == False:
+#     import django_on_heroku
+#     django_on_heroku.settings(locals())
 
 
 
